59b89bb905cf50ef5a4debbf039485abe92f350a
marianovba <mariano.valenzuela945@gmail.com>
2023-08-14 20:53:00 -0700

    Intento Push desde VSCode

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-waaaaaaaaaaaaaaaaaaf
-Intento de push Mariano
+print("waaaaaaaaaaaaaaaaaaf")
+print("Intento de push Mariano")
 print('hola -m')
 print("hola 2 -m")
 print("in the arms of the angel flay awaaaaaaaaay form here")
+
+#comentario prueba
