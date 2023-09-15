e494cf188c6abd7e42e7d6e81cf3213496f1e192
LiveAnywhereElse <yaelitamarvayala@gmail.com>
2023-09-15 16:15:54 -0700

    Esto sigue sin correr, pero quiero creer que esta mas cerca

--- ml23/ejercicio_3_NaiveBayes/naive_bayes.py
+++ ml23/ejercicio_3_NaiveBayes/naive_bayes.py
@@ -6,18 +6,29 @@ class NaiveBayes():
 
     def fit(self, X, y):
         # TODO: Calcula la probabilidad de que una muestra sea positiva P(y=1)
-        # self.prior_positives = P(y > 1|X) = (P(X|y)*P(y))/P(X)
-        self.prior_negative = self(y > 1|X) = (self(X|y)*self(y))/self(X)
+        #self.prior_positives = P(y > 1|X) = (P(X|y)*P(y))/P(X)
+        #self.prior_positives = P(X|y)*P(y))/P(X) 
+        #self.prior_positives = P(X n y)/P(X)*P(y))/P(X)
+        self.prior_positives = (((X*y)/self*self)/((X/self)*(y/self)))/(X/self)
+        #self.prior_negative = self(y > 1|X) = (self(X|y)*self(y))/self(X)
 
         # TODO: Calcula la probabilidad de que una muestra sea negativa P(y=0)
         # self.prior_negative = P(y < 1|X) = (P(X|y)*P(y))/P(X)
-        self.prior_negative = self(y < 1|X) = (self(X|y)*self(y))/self(X)
+        self.prior_negatives = (self*((X*y)/self*self)/(self*(X/self)*(y/self)))/(X/self)
+        #self.prior_negative = self(y < 1|X) = (self(X|y)*self(y))/self(X)
 
         # TODO: Para cada palabra del vocabulario x_i
         # calcula la probabilidad de: P(x_i| y=1)
         # Guardalas en un arreglo de numpy:
-        # self._likelihoods_positives = [P(x_1| y=1), P(x_2| y=1), ..., P(x_n| y=1)]
-        # self._likelihoods_positives = 
+        
+        #for word in words:
+            #self.prior_negative = self(y > 1|X) = (self(X|y)*self(y))/self(X)
+            #self.prior_negative = self(y < 1|X) = (self(X|y)*self(y))/self(X)
+            
+        
+        
+        #self._likelihoods_positives = [P(x_1| y=1), P(x_2| y=1), ..., P(x_n| y=1)]
+        #self._likelihoods_positives = 
         
         # TODO:  Para cada palabra del vocabulario x_i, calcula P(x_i| y=0)
         # Guardalas en un arreglo de numpy:
