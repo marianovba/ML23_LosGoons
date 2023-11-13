9c5730bd5e720b778bff82f22f3019436febaa1f
marianovba <mariano.valenzuela945@gmail.com>
2023-11-12 16:46:49 -0800

    network

--- ml23/proyecto_2_DL/network.py
+++ ml23/proyecto_2_DL/network.py
@@ -12,9 +12,13 @@ class Network(nn.Module):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         # TODO: Calcular dimension de salida
-        out_dim = ...
+        out_dim = self.calc_out_dim(input_dim, kernel_size=3, stride=1, padding=0)
 
         # TODO: Define las capas de tu red
+        self.conv1 = nn.Conv2d(in_channels=input_dim, out_channels=64, kernel_size=3)
+        self.conv1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3)
+        self.linear1 = nn.Linear(out_dim, 512)
+        self.linear2 = nn.Linear(512, n_classes)
 
         self.to(self.device)
  
@@ -24,6 +28,15 @@ class Network(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # TODO: Define la propagacion hacia adelante de tu red
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.linear1(x)
+        x = F.relu(x)
+        logits = self.linear2(x)
+        proba = F.softmax(logits, dim=1)
         return logits, proba
 
     def predict(self, x):
@@ -39,7 +52,7 @@ class Network(nn.Module):
         '''
         models_path = file_path / 'models' / model_name
         # TODO: Guarda los pesos de tu red neuronal en el path especificado
-        torch.save( ... )
+        torch.save( self.state_dict(), models_path)
 
     def load_model(self, model_name: str):
         '''
@@ -48,3 +61,5 @@ class Network(nn.Module):
             - path (str): path relativo donde se guardó el modelo
         '''
         # TODO: Carga los pesos de tu red neuronal
+        models_path = file_path / 'models' / model_name
+        self.load_state_dict(torch.load(models_path))
